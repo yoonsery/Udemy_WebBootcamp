7bd02916299eaa2cb6e6ce65c1e9431073d5626b
sery <yunsel2@gmail.com>
2021-11-03 00:21:09 +0900

    feat: Install body-parser & Processing post requests with body parser

--- challenge/calculator/calculator.js
+++ challenge/calculator/calculator.js
@@ -1,9 +1,21 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 const app = express();
 const port = 3000;
 
+app.use(bodyParser.urlencoded({ extended: true }));
+
 app.get('/', (req, res) => {
   res.sendFile(`${__dirname}/index.html`);
+});
+
+app.post('/', (req, res) => {
+  const num1 = Number(req.body.num1);
+  const num2 = Number(req.body.num2);
+
+  const result = num1 + num2;
+
+  res.send(`The result of calculation is ${result}`);
 });
 
 app.listen(port, () => {
