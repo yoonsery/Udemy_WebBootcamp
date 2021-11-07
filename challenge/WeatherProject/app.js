0207e03987ff53cdcd05b0d00c54216866930c59
sery <yunsel2@gmail.com>
2021-11-07 21:30:31 +0900

    how to parse JSON

--- challenge/WeatherProject/app.js
+++ challenge/WeatherProject/app.js
@@ -8,9 +8,15 @@ app.get('/', (req, res) => {
     'https://api.openweathermap.org/data/2.5/weather?q=London&appid=727882c6eaab040ebe0ae213b5b5fcaf&units=metric';
 
   https.get(url, (response) => {
-    console.log(response);
-    // console.log('statusCode:', response.statusCode);
-    // console.log('hearders:', response.headers);
+    console.log('statusCode:', response.statusCode);
+
+    response.on('data', (data) => {
+      const weatherData = JSON.parse(data);
+      const temp = weatherData.main.temp;
+      const weatherDescription = weatherData.weather[0].description;
+      console.log(temp);
+      console.log(weatherDescription);
+    });
   });
 
   res.send('Server is up and running');
