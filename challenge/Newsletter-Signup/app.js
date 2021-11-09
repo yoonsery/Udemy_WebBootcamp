cf23ce9ded356bbd0fe1a4f743f0fac4ae849a6f
sery <yunsel2@gmail.com>
2021-11-09 21:59:32 +0900

    feat: Add public folder and categorize

--- challenge/Newsletter-Signup/app.js
+++ challenge/Newsletter-Signup/app.js
@@ -1,13 +1,20 @@
 const express = require('express');
-const bodyParser = require('body-parser');
+const request = require('request');
 const dotenv = require('dotenv');
 
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded());
+
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
-  res.send('server is running');
+  res.sendFile(`${__dirname}/signup.html`);
+  
+  
 });
 
 app.listen(3000, () => {
   console.log(`Server is running on port 3000`);
+  
 });
