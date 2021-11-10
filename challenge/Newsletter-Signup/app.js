c2e73bac47ac636678ae0e810a009464884f6628
sery <yunsel2@gmail.com>
2021-11-10 20:31:13 +0900

    feat: Posting data to mailchimp's server via latest updated API

--- challenge/Newsletter-Signup/app.js
+++ challenge/Newsletter-Signup/app.js
@@ -1,20 +1,55 @@
 const express = require('express');
 const request = require('request');
 const dotenv = require('dotenv');
+const mailchimp = require('@mailchimp/mailchimp_marketing');
 
 const app = express();
-app.use(express.json());
-app.use(express.urlencoded());
+
+dotenv.config();
+
+mailchimp.setConfig({
+  apiKey: process.env.MAILCHIMP_API_KEY,
+  server: 'us20',
+});
 
 app.use(express.static('public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
   res.sendFile(`${__dirname}/signup.html`);
-  
-  
+});
+
+app.post('/', (req, res) => {
+  const firstName = req.body.fName;
+  const lastName = req.body.lName;
+  const email = req.body.email;
+
+  const listId = process.env.MAILCHIMP_ID;
+  const subscribingUser = {
+    firstName,
+    lastName,
+    email,
+  };
+
+  async function run() {
+    const response = await mailchimp.lists.addListMember(listId, {
+      email_address: subscribingUser.email,
+      status: 'subscribed',
+      merge_fields: {
+        FNAME: subscribingUser.firstName,
+        LNAME: subscribingUser.lastName,
+      },
+    });
+
+    console.log(
+      `Successfully added contact as an audience member. The contact's id is ${response.id}.`
+    );
+  }
+
+  run();
 });
 
 app.listen(3000, () => {
   console.log(`Server is running on port 3000`);
-  
 });
