db85a8748fcbd8860eb42213c72ca2aad14ff5be
sery <yunsel2@gmail.com>
2021-11-11 18:47:52 +0900

    feat: Edit port address and make Procfile

--- challenge/Newsletter-Signup/app.js
+++ challenge/Newsletter-Signup/app.js
@@ -52,6 +52,6 @@ app.post('/failure', (req, res) => {
   res.redirect('/');
 });
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log(`Server is running on port 3000`);
 });
