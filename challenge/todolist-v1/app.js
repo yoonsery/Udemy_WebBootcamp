e104231874d528397f4f8d3407e8a820458dd6e5
sery <yunsel2@gmail.com>
2021-11-19 00:02:57 +0900

    feat: How to use layouts in EJS, adapt header, footer

--- challenge/todolist-v1/app.js
+++ challenge/todolist-v1/app.js
@@ -41,6 +41,10 @@ app.get('/work', (req, res) => {
   res.render('list', { listTitle: 'Work List', newListItems: workItems });
 });
 
+app.get('/about', (req, res) => {
+  res.render('about');
+});
+
 app.listen(3000, () => {
   console.log('Server started on port 3000');
 });
