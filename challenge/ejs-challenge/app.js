86a514dfbc9bb150170d26369a8de6c401e9448f
sery <yunsel2@gmail.com>
2021-11-25 19:03:56 +0900

    feat: Delete log and pass posts to home.ejs page

--- challenge/ejs-challenge/app.js
+++ challenge/ejs-challenge/app.js
@@ -21,8 +21,7 @@ app.use(express.static('public'));
 const posts = [];
 
 app.get('/', (req, res) => {
-  console.log(posts);
-  res.render('home', { homeContent: homeStartingContent });
+  res.render('home', { homeContent: homeStartingContent, posts });
 });
 
 app.get('/about', (req, res) => {
