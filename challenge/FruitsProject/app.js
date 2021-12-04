61b9c6ff97dcd3cd7cf0745fa0a50e13b68f5282
sery <yunsel2@gmail.com>
2021-12-05 00:45:16 +0900

    feat: Set the fruitProject to exercise mongodb native driver

---
+++ challenge/FruitsProject/app.js
@@ -0,0 +1,44 @@
+const { MongoClient } = require('mongodb');
+
+const uri = 'mongodb://localhost:27017';
+// const uri =
+// 'mongodb+srv://<user>:<password>@<cluster-url>?retryWrites=true&writeConcern=majority';
+
+const client = new MongoClient(
+  uri, //
+  { useUnifiedTopology: true }
+);
+
+async function run() {
+  try {
+    await client.connect();
+    console.log('Connected Successfully to server');
+
+    const database = client.db('fruitsDB');
+    const fruitsCollection = database.collection('fruits');
+
+    const fruits = [
+      {
+        name: 'Apple',
+        score: 8,
+        review: 'Great fruit',
+      },
+      {
+        name: 'Orange',
+        score: 6,
+        review: 'Kinda sour',
+      },
+      {
+        name: 'Banana',
+        score: 9,
+        review: 'Great stuff!',
+      },
+    ];
+
+    const result = await fruitsCollection.insertMany(fruits);
+    console.log(result.insertedCount);
+  } finally {
+    await client.close();
+  }
+}
+run().catch(console.dir);
