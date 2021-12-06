4af47969f5bbd68a799439a18dcad8d7fb5118a8
sery <yunsel2@gmail.com>
2021-12-07 00:28:43 +0900

    feat: How to delete data and challenge to delete all documents

--- challenge/FruitsProject/app.js
+++ challenge/FruitsProject/app.js
@@ -83,14 +83,47 @@ Fruit.find((err, fruits) => {
 
 // 📌 updata data
 
-Fruit.updateOne(
-  { _id: '61ae22e18d1359358fe86ea3' },
-  { name: 'blueberry' },
-  (err) => {
-    if (err) {
-      console.log(err);
-    } else {
-      console.log('Succesfully updated the document');
-    }
+// Fruit.updateOne(
+//   { _id: '61ae22e18d1359358fe86ea3' },
+//   { name: 'blueberry' },
+//   (err) => {
+//     if (err) {
+//       console.log(err);
+//     } else {
+//       console.log('Succesfully updated the document');
+//     }
+//   }
+// );
+
+// 📌 delete specific documents from the collection
+
+Fruit.deleteOne({ name: 'blueberry' }, (err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log('Succesfully deleted!');
   }
-);
+});
+
+// challenge
+
+const personSchema = new mongoose.Schema({
+  name: String,
+  age: Number,
+});
+
+const Person = mongoose.model('person', personSchema);
+const person = new Person({
+  name: 'John',
+  age: 37,
+});
+
+person.save();
+
+Person.deleteMany({ name: 'John' }, (err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log('Succesfully deleted all!');
+  }
+});
