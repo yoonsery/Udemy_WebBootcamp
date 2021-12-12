e47f4bd07b8555e521bb7cf31c6b8e1c311fef0f
sery <yunsel2@gmail.com>
2021-12-12 21:16:17 +0900

    feat: Install lodash, capitalize custom todolist

--- challenge/todolist-v1/app.js
+++ challenge/todolist-v1/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const _ = require('lodash');
 
 const app = express();
 
@@ -58,7 +59,7 @@ app.get('/', (req, res) => {
 });
 
 app.get('/:customListName', (req, res) => {
-  const customListName = req.params.customListName;
+  const customListName = _.capitalize(req.params.customListName);
 
   List.findOne({ name: customListName }, (err, foundList) => {
     if (!err) {
