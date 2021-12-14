2993a2a934a3525f5f42fce167b1b9ba7df3a1b0
sery <yunsel2@gmail.com>
2021-12-15 01:46:22 +0900

    feat: Add procfile & update ports

--- challenge/todolist-v1/app.js
+++ challenge/todolist-v1/app.js
@@ -141,6 +141,11 @@ app.get('/about', (req, res) => {
   res.render('about');
 });
 
-app.listen(3000, () => {
-  console.log('Server started on port 3000');
+let port = process.env.PORT;
+if (port == null || port == '') {
+  port = 3000;
+}
+
+app.listen(port, () => {
+  console.log('Server has started successfully');
 });
