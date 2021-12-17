97979bc215295d78c412ca924396be993f7db97b
sery <yunsel2@gmail.com>
2021-12-17 23:18:44 +0900

    feat: Fix typo

--- challenge/ejs-challenge/app.js
+++ challenge/ejs-challenge/app.js
@@ -2,9 +2,9 @@ const express = require('express');
 const ejs = require('ejs');
 const Mongoose = require('mongoose');
 const _ = require('lodash');
-const dotevn = require('dotenv');
+const dotenv = require('dotenv');
 
-dotevn.config();
+dotenv.config();
 
 const homeStartingContent =
   'Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.';
