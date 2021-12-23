3c35e61d46fa029900e2d2a2f23b334b2f46aae6
sery <yunsel2@gmail.com>
2021-12-23 22:47:21 +0900

    feat: Connect mongoose, Add get, post, delete

--- challenge/wiki-api/app.js
+++ challenge/wiki-api/app.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const ejs = require('ejs');
-const _ = require('lodash');
 const dotenv = require('dotenv');
 
 const app = express();
@@ -13,12 +12,56 @@ app.use(express.static('public'));
 
 dotenv.config();
 
+async function connectDB() {
+  await mongoose.connect('mongodb://localhost:27017/wikiDB', {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
+}
+
+connectDB() //
+  .catch((err) => console.log(err));
+
 const articleSchema = new mongoose.Schema({
   title: String,
   content: String,
 });
 
-const Article = mongoose.model('article', articleSchema);
+const Article = mongoose.model('Article', articleSchema);
+
+app.get('/articles', (req, res) => {
+  Article.find((err, foundArticles) => {
+    if (!err) {
+      res.send(foundArticles);
+    } else {
+      res.send(err);
+    }
+  });
+});
+
+app.post('/articles', (req, res) => {
+  const newArticle = new Article({
+    title: req.body.title,
+    content: req.body.content,
+  });
+  newArticle.save((err) => {
+    if (!err) {
+      res.send('Successfully added a new article');
+    } else {
+      res.send(err);
+    }
+  });
+});
+
+app.delete('/articles', (req, res) => {
+  Article.deleteMany((err) => {
+    if (!err) {
+      res.send('Successfully deleted all articles');
+    } else {
+      res.send(err);
+    }
+  });
+});
 
 app.listen(3000, () => {
   console.log('Server started on port 3000');
