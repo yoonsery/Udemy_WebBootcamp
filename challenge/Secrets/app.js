4a446231ae15d41889e13b084d89a4110f1efe08
sery <yunsel2@gmail.com>
2022-01-03 11:27:26 +0900

    Set up for authentication and security challenge project

---
+++ challenge/Secrets/app.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const ejs = require('ejs');
+
+const app = express();
+
+app.use(express.static('public'));
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.render('home');
+});
+
+app.get('/login', (req, res) => {
+  res.render('login');
+});
+
+app.get('/register', (req, res) => {
+  res.render('register');
+});
+
+app.listen(3000, () => {
+  console.log('Server started on port 3000');
+});
