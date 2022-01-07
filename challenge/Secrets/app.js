1db3141995bf925692d8cd57ced279cb3ce66023
sery <yunsel2@gmail.com>
2022-01-07 23:44:58 +0900

    feat: Install session, passport, passport-local-mongoose

--- challenge/Secrets/app.js
+++ challenge/Secrets/app.js
@@ -2,8 +2,9 @@ require('dotenv').config();
 const express = require('express');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
-const bcrypt = require('bcrypt');
-const saltRounds = 10;
+const session = require('express-session');
+const passport = require('passport');
+const passportLocalMongoose = require('passport-local-mongoose');
 
 const app = express();
 
@@ -38,42 +39,13 @@ app.get('/login', (req, res) => {
   res.render('login');
 });
 
-app.post('/login', (req, res) => {
-  const username = req.body.username;
-  const password = req.body.password;
-
-  User.findOne({ email: username }, (err, foundUser) => {
-    if (err) {
-      console.log(err);
-    } else {
-      if (foundUser) {
-        bcrypt.compare(password, foundUser.password, (err, result) => {
-          result && res.render('Secrets');
-        });
-      }
-    }
-  });
-});
+app.post('/login', (req, res) => {});
 
 app.get('/register', (req, res) => {
   res.render('register');
 });
 
-app.post('/register', (req, res) => {
-  bcrypt.hash(req.body.password, saltRounds, (err, hash) => {
-    const newUser = new User({
-      email: req.body.username,
-      password: hash,
-    });
-    newUser.save((err) => {
-      if (err) {
-        console.log(err);
-      } else {
-        res.render('secrets');
-      }
-    });
-  });
-});
+app.post('/register', (req, res) => {});
 
 app.listen(3000, () => {
   console.log('Server started on port 3000');
